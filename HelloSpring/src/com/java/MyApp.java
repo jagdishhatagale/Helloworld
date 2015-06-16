3b6d56df029134160336771bf3e36b49cfcfd9d9
riyakaur <riya.kaur83@gmail.com>
2015-06-16 16:26:23 +0530

    added 1

--- HelloSpring/src/com/java/MyApp.java
+++ HelloSpring/src/com/java/MyApp.java
@@ -8,7 +8,7 @@ public class MyApp {
 		// TODO Auto-generated method stub
 		ApplicationContext context= new ClassPathXmlApplicationContext("Bean.xml");
 		HelloWorld obj= (HelloWorld) context.getBean("helloWorld");
-		System.out.println("The msg after GIT");
+		System.out.println("The msg after GIT.........1");
 		obj.getMessage();
 
 	}
